a17cd9caa129e48df74b3540c269f95d86dc37e1
rsakao-san <sakao@sansan.com>
2024-11-06 13:13:56 +0900

    単体テストに移動したテスト処理を削除

--- python/calculate_average.py
+++ python/calculate_average.py
@@ -4,8 +4,3 @@ def calculate_average(numbers):
         total += number
     average = total / len(numbers)
     return average
-
-
-# テスト用のリスト
-numbers = [1, 2, 3, 4, 5]
-print("Average:", calculate_average(numbers))
