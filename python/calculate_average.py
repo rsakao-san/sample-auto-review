ae18472259d611507acd9467209632d5900d7944
rsakao-san <sakao@sansan.com>
2024-11-06 10:45:08 +0900

    add sample code python

---
+++ python/calculate_average.py
@@ -0,0 +1,10 @@
+def calculate_average(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    average = total / len(numbers)
+    return average
+
+# テスト用のリスト
+numbers = [1, 2, 3, 4, 5]
+print("Average:", calculate_average(numbers))
